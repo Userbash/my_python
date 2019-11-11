8d8982b5283e48751db6522d51ccfcd5eb3c6df3
alex <rubaka96@yandex.ru>
2019-11-11 18:01:45 +0500

    not work

--- test.sh
+++ test.sh
@@ -1,47 +1,33 @@
 #!/bin/bash
 
-HOME=$(echo | awk -v home=$HOME '{print "My home is " home}' |awk {'print$4'});
-
 test(){
-  
-  dt=$(date -dm |awk '{print$2}')
-
-num(){
-
- rp=$(($RANDOM %512));
-  
-  for d in $rp $rp; do k=$(echo $d | awk {'print$1'} | tail -n1); if [ -z $k ] || [ $k -eq 0 ]; then return; else echo ""; fi; done
-   
-}
-
-list(){
-   
-   mount -t cifs //192.168.XX.1XX/Users1 -o dom=megaflowers.local,username=operator,pass=XXXX $HOME/tmp &&\
-   cp -rvf $HOME/tmp/ $PATCH > log &&\
-   sleep 300 &&\
-   umount -l  $HOME/tmp&&\
-   rm -rf $HOME\tmp
+ . env_per
+num(){  
+  for d in $rp $rp; do k=$(echo $d | awk {'print$1'} | tail -n1); if [ -z $k ] || [ $k -eq 0 ]; then return; else echo ""; fi; done   
 }
 
 backup(){
-   mkdir $HOME/tmp
-   dt=$(date +"%m-%d-%Y");
-   sd=$(echo $dt | sed -e 's/\(.*\)-\(.*\)-\(.*\)/\3-\2-\1/')
-   fin=$( find $PATCH -type f -newermt 2019-06-01 ! -newermt $sd)
-  if [ -z  fin ]
-    then
-      return
-   else   	
-       dir=$(if [ -d $PATCH ]; then list; else return; fi) 
-        if [ -d  $HOME/tmp ]; then return; else $dir; fi     	
-   fi   	
+   . env_per
+     if [ -d $HOME/tmp ]
+       then 
+         if [ -z  fin ]
+            then
+             return
+         else     
+             dir=$(if [ -d $PATCH ]; then . env_copy; else return; fi) 
+             if [ -d  $HOME/tmp ]; then return; else $dir; fi 
+          fi 
+    else
+        mkdir $HOME/tmp
+         if backup; then echo "return funcion backup"; fi
+    fi         
 }
 
 mass(){
   files=($dates)
         if [ -z $files ]; then backup; else echo "OK"; exit 0; fi 	
-}
-
+} 
+  
   if [ -z $HOME ];
   	 then
   	 	return
@@ -51,4 +37,4 @@ mass(){
   fi
 }
 
-if PATCH=/media/files; then test; fi 
+if $PATCH; then test; fi 
